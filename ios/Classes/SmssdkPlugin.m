02fecece2608fec8928d1245d155c38bdc9c1699
once10301 <175915345@qq.com>
2019-03-21 14:56:17 +0800

    add iOS

--- ios/Classes/SmssdkPlugin.m
+++ ios/Classes/SmssdkPlugin.m
@@ -1,20 +1,42 @@
 #import "SmssdkPlugin.h"
+#import <SMS_SDK/SMSSDK.h>
 
 @implementation SmssdkPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"smssdk"
-            binaryMessenger:[registrar messenger]];
-  SmssdkPlugin* instance = [[SmssdkPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"smssdk"
+                                     binaryMessenger:[registrar messenger]];
+    SmssdkPlugin* instance = [[SmssdkPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"getCode" isEqualToString:call.method]) {
+        NSDictionary* argsMap = call.arguments;
+        NSString *phone = argsMap[@"phone"];
+        [SMSSDK getVerificationCodeByMethod:SMSGetCodeMethodSMS phoneNumber:phone zone:@"86" template:@"" result:^(NSError *error) {
+            if (!error) {
+                NSDictionary *dic = @{@"status":@0,@"msg":@"success",};
+                result(dic);
+            } else {
+                NSDictionary *dic = @{@"status":@1,@"msg":error.userInfo[@"description"],};
+                result(dic);
+            }
+        }];
+    } else if ([@"submitCode" isEqualToString:call.method]) {
+        NSDictionary* argsMap = call.arguments;
+        NSString *phone = argsMap[@"phone"];
+        NSString *code = argsMap[@"code"];
+        [SMSSDK commitVerificationCode:code phoneNumber:phone zone:@"86" result:^(NSError *error) {
+            if (!error) {
+                NSDictionary *dic = @{@"status":@0,@"msg":@"success",};
+                result(dic);
+            } else {
+                NSDictionary *dic = @{@"status":@1,@"msg":error.userInfo[@"description"],};
+                result(dic);
+            }
+        }];
+    }
 }
 
 @end
